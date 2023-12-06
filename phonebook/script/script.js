59eeebc607905e08e169c21fff1791284223fde8
Vasilii Verkin <v.verkin@gmail.com>
2023-12-07 00:02:12 +0300

    оформил телефонный справочник

--- phonebook/script/script.js
+++ phonebook/script/script.js
@@ -1,5 +1,28 @@
 'use strict';
 
+const data = [
+  {
+    name: 'Иван',
+    surname: 'Петров',
+    phone: '+79514545454',
+  },
+  {
+    name: 'Игорь',
+    surname: 'Семёнов',
+    phone: '+79999999999',
+  },
+  {
+    name: 'Семён',
+    surname: 'Иванов',
+    phone: '+79800252525',
+  },
+  {
+    name: 'Мария',
+    surname: 'Попова',
+    phone: '+79876543210',
+  },
+];
+
 /*Создаем свою область видимости, 
 чтобы ничего не выходило в глобальную область, 
 кроме того, что необходимо вынести*/
@@ -30,37 +53,236 @@
     // Возвращаем header
     return header;
   };
-   // Ф-я добавляет логотип
+   // Ф-я добавляет логотип в виде загловка
   const createLogo = title => {
+    // Создаем в верстке эл-т h1
     const h1 = document.createElement('h1');
+    // Присваиваем эл-ту класс logo
     h1.classList.add('logo');
+    // Добавляем содержимое в заголовок
     h1.textContent = `Телефонный справочник. ${title}`;
 
     return h1;
   };
+  // Создадим ф-ю main
+  const createMain = () => {
+    const main = document.createElement('main');
+    // Создаем контейнер для main
+    const mainContainer = createContainer();
+    // Вставляем контейнер
+    main.append(mainContainer);
+    // Добавим такое же св-во как у хедер, что бы можно было вставлять в него элементы
+    main.mainContainer = mainContainer;
 
-const createMain = () => {
-  const main = document.createElement('main');
-  const mainContainer = createContainer();
-  main.append(mainContainer);
-  main.mainContainer = mainContainer;
+    return main;
+  };
 
-  return main;
-};
+  // Ф-я создает кнопки, принимает массив params, в котором может быть несколько кнопок
+  const createButtonsGroup = params => {
+    // Создаем обертку для кнопок
+    const btnWrapper = document.createElement('div');
+    // Добавляем класс для обертки
+    btnWrapper.classList.add('btn-wrapper');
+    //params - массив, перебераем его с помощью map
+    //map передаем данные, на основе которых будут получаться кнопки
+    //в скобках деструктурируем className, type, text
+    const btns = params.map(({className, type, text}) => {
+    // Создаем сами кнопки
+    const button = document.createElement('button');
+    // Заполняем кнопки данными
+    button.type = type;
+    button.textContent = text;
+    button.className = className;
+    // Возвращаем получившуюся кнопку
+    return button;
+    });
+    // Вставляем btns в btnWrapper
+  btnWrapper.append(...btns); // Т.к. мы не можем вставить массив в обертку,
+                              // мы раскладываем его с помощью спред-оператора
+
+    return {
+      btnWrapper,
+      btns,
+    };
+  };
+
+  //Ф-я создает таблицу
+  const createTable = () => {
+    const table = document.createElement('table');
+    // Назначаем классы по bootstrap
+    table.classList.add('table', 'table-striped');
+    // Создаем thead для заголовков таблицы
+    const thead = document.createElement('thead');
+    // Вставляем верстку шапки таблицы
+    thead.insertAdjacentHTML('beforeend', `
+    <tr> 
+    <th class="delete">Удалить</th>
+    <th>Имя</th>
+    <th>Фамилия</th>
+    <th>Телефон</th>
+    </tr>
+    `); // class="delete" - делает элемент скрытым
+
+    // Создаем тело таблицы
+    const tbody = document.createElement('tbody');
+    // tbody ничего не содержит, поэтому просто вставляем в таблицу
+    table.append(thead, tbody); // Важен порядок вставки
+    // Что бы не возвращать tbody как объект, в сам элемент table добавим свойство tbody (как в контейнер)
+    table.tbody = tbody;
+
+    return table;
+  };
+
+  // Ф-я зоздает форму для добавления данных
+  const createForm = () => {
+    // Почти все формы в модальном окне, поэтому создадим overlay для модального окна
+    const overlay = document.createElement('div');
+    // Задаем класс 
+    overlay.classList.add('form-overlay');
+    // Создаем форму
+    const form = document.createElement('form');
+    // Добавляем класс форме
+    overlay.classList.add('form-overlay');
+    // Форма статичная, поэтому просто вставляем верстку
+    form.insertAdjacentHTML('beforeend', `
+    <button class ="close" type="button"></button>
+    <h2 class="form-title">Добавить контакт</h2>
+    <div class = "form-group">
+      <label class="form-label" for="name">Имя:</label>
+      <input class="form-input" name="name" id="name" type="text" required>
+    </div>
+    <div class = "form-group">
+    <label class="form-label" for="surname">Фамилия:</label>
+    <input class="form-input" name="surname" id="surname" type="text" required>
+  </div>
+  <div class = "form-group">
+  <label class="form-label" for="phone">Телефон:</label>
+  <input class="form-input" name="phone" id="phone" type="number" required>
+</div>
+    `);
+// Добавляем кнопки в форму
+const buttonGroup = createButtonsGroup ([
+  {
+    className: 'btn btn-primary mr-3', // класс кнопки 1. mr-3 отступ по bootstrap
+    type: 'submit', // тип кнопки 1
+    text: 'Добавить', // текст кнопки 1
+  },
+  {
+    className: 'btn btn-danger', // класс кнопки 2
+    type: 'reset', // тип кнопки 2
+    text: 'Отмена', // текст кнопки 2
+  },
+]);
+//Добавляем в форму кнопки, которые достаем из массива btns
+    form.append(...buttonGroup.btns);
+// Добавляем форму в overlay
+    overlay.append(form);
+    // Можно вернуть кнопки, что бы можно было навешать на них события, но у нас это не требуется.
+    // Мы вернем overlay и форму
+    return {
+      overlay,
+      form,
+    };
+  };
 
 
-  // Ф-я, конорая инициализирует наше приложение
-  const init = (selectorApp, title) => {
-    // Получим элемент по селектору
-    const app = document.querySelector(selectorApp);
+ // Основная функция
+  const renderPhoneBook = (app, title) => {
+
     // Создаем хедер страницы
     const header = createHeader();
-    // Добавляем лого в хедер
+    // Добавляем заголовок
     const logo = createLogo(title);
-    // Добавляем лого в хедер
-    header.append(logo);
-    // Добавляем header на страницу
-    app.apend(header);
+    // Принимаем main
+    const main = createMain();
+    // Принимаем кнопки
+    // в скобках указываем параметры, которые будем передавать в params.map({})
+    const buttonGroup = createButtonsGroup([
+      {
+        className: 'btn btn-primary mr-3', // класс кнопки 1. mr-3 отступ по bootstrap
+        type: 'button', // тип кнопки 1
+        text: 'Добавить', // текст кнопки 1
+      },
+      {
+        className: 'btn btn-danger', // класс кнопки 2
+        type: 'button', // тип кнопки 2
+        text: 'Удалить', // текст кнопки 2
+      },
+    ]);
+    // Создаем переменную для таблицы
+    const table = createTable();
+    // Вызываем форму
+    const form = createForm();
+    // Добавляем кнопки,таблицу и оверлай в main
+    main.mainContainer.append(buttonGroup.btnWrapper, table, form.overlay);
+    // Добавляем добавляем заголовок в контейнер хедера
+    header.headerContainer.append(logo);
+    // Добавляем header и main на страницу, в которые мы сможем добавлять элементы
+
+    app.append(header, main);
+
+    // Основная функция возвращает объект с необходимыми нам элементами
+    return {
+      //tbody можно вывести только черерез свойство лист в объекте
+      list: table.tbody,
+    };
+  };
+// Ф-я создает строку на основе данных из объекта. Можно так:   const createRow = dataObj, но лучше через деструктуризацию
+   // В скобках сразу проводим деструктуризацию
+  const createRow = ({name: firstName, surname, phone}) => { // Переименовываем name в firatName, т.к. в глобальной области видимости уже есть name 
+  // Создаем строку
+  const tr = document.createElement('tr');
+
+// Создаем ячейки
+    // в tdDel данных нет, есть кнопка. 
+    const tdDel = document.createElement('td');
+    // Кнопки изначально скрыты, поэтому назначаем им класс delete
+    tdDel.classList.add('delete');
+    // Создаем кнопку
+    const buttonDel = document.createElement('button');
+    //Появившиеся кнопки оформляем подготовленным классом
+    buttonDel.classList.add('del-icon');
+    //в ячейку tdDel вставляем кнопку
+    tdDel.append(buttonDel);
+
+    
+    // Оформляем омтальные элементы
+
+    const tdName = document.createElement('td');
+    tdName.textContent = firstName; // В качестве контента берем деструктурированные данные
+    const tdSurname = document.createElement('td');
+    tdSurname.textContent = surname; // В качестве контента берем деструктурированные данные
+    const tdPhone = document.createElement('td');
+    //Создаем ссылку, что бы клике на телефон открывалась звонилка
+    const phoneLink = document.createElement('a');
+    phoneLink.href = `tel:${phone}`;
+    phoneLink.textContent = phone; // В качестве контента берем деструктурированные данные
+    // В tdPhone дщбавляем нашу ссылку
+    tdPhone.append(phoneLink);
+
+    // Вставляем td в tr
+    tr.append(tdDel, tdName, tdSurname, tdPhone);
+    // Возвращаем получившуюся строку
+    return tr;
+  };
+  // Ф-я принимает элемент и массив с объектами
+  const renderContacts = (elem, data) => {
+    // Создаем элементы перебирая массив с объектами
+    const allRow = data.map(createRow);
+    // выводим результат на страницу
+    elem.append(...allRow);
+  };
+ // Ф-я, конорая инициализирует наше приложение
+  const init = (selectorApp, title) => {
+    // Получим элемент по селектору и передадим в ф-ю render
+    const app = document.querySelector(selectorApp);
+    // Вызываем основную функцию и передаем в нее app и title
+    const phoneBook = renderPhoneBook(app, title);
+    // Выполним деструктуризацию list из phoneBook
+    const {list} = phoneBook;
+    //В ф-ю передаем list в чистом виде после деструктуризации и data
+    renderContacts(list, data);
+    // Функционал
   };
 
   window.phoneBookInit = init;
