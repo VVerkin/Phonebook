8e75a9cf4cf8fffb8c250379cc21236c8c7d61a3
Vasilii Verkin <v.verkin@gmail.com>
2023-12-26 23:33:41 +0300

    вынесены функции открытия и закрытия формы

--- phonebook/script/script.js
+++ phonebook/script/script.js
@@ -243,10 +243,11 @@ const buttonGroup = createButtonsGroup ([
     // Вызов ф-ии, создания футера
     const footer = createFooter();
     const copyRight = createCopyRight(title);
-    // Вызываем форму
-    const form = createForm();
+    // Получаем объект, который деструктурируем на form и overlay.
+    // Таким образом мы сможем сразу использовать данные идентификаторы
+    const {form, overlay} = createForm();
     // Добавляем кнопки,таблицу и оверлай в main
-    main.mainContainer.append(buttonGroup.btnWrapper, table, form.overlay);
+    main.mainContainer.append(buttonGroup.btnWrapper, table, overlay);
     // Добавляем добавляем заголовок в контейнер хедера
     header.headerContainer.append(logo);
     // Добавляем элементы header, main и footer на страницу, в которые мы сможем добавлять содержимое
@@ -262,8 +263,8 @@ const buttonGroup = createButtonsGroup ([
       btnAdd: buttonGroup.btns[0],
       // Возвращаем кнопку "Удалить"
       btnDel: buttonGroup.btns[1],
-      formOverlay: form.overlay,
-      form: form.form,
+      formOverlay: overlay,
+      form,
     };
   };
 // Ф-я создает строку на основе данных из объекта. Можно так:   const createRow = dataObj, но лучше через деструктуризацию
@@ -335,41 +336,63 @@ const buttonGroup = createButtonsGroup ([
   };
   // Ф-я открывает модальное окно с формой при нажатии на кнопку "Добавить"
   const modalControl = (btnAdd, formOverlay) => {
-    // Навешиваем событие при клике на кнопку
-    btnAdd.addEventListener('click', () => {
+    // Ф-я добавляет класс для открыттия формы
+    const openModal = () => {
       formOverlay.classList.add('is-visible');
-    });
-// Ф-я закрывает форму при клике на overlay или на крестик
+    };
+    // Ф-я убирает класс для закрытия формы
+    const closeModal = () => {
+      formOverlay.classList.remove('is-visible');
+    };
+    // Навешиваем событие при клике на кнопку
+    btnAdd.addEventListener('click', openModal);
+    // Ф-я закрывает форму при клике на overlay или на крестик
     formOverlay.addEventListener('click', e => {
       const target = e.target; // создаем переменную target что бы дальше не писать event.target
       // Проверяем, что target - это formOverlay
-      if (target === formOverlay || target.classList.contains('close')) {
-        // Если да, то закрываем форму
-        formOverlay.classList.remove('is-visible');
+      if (target === formOverlay || target.closest('.close')) {
+        // Если да, то вызываем функцию закрытия формы
+        closeModal();
       }
     });
+    // Возвращаем ф-ю закрытия формы
+    return {
+      closeModal,
+    };
   };
-// Ф-я при нажатии на кнопку "Удалить" показывает крестики, при нажатии на которые удаляется строка
-const deleteControl = (btnDel, list) => {
-// Навешиваем событие на кнопку "Удалить"
-btnDel.addEventListener('click', () => {
-  // Проходим по таблице, находим элементы с классом delete и показываем их
-  document.querySelectorAll('.delete').forEach(del => {
-    del.classList.toggle('is-visible');
+  // Ф-я при нажатии на кнопку "Удалить" показывает крестики, при нажатии на которые удаляется строка
+  const deleteControl = (btnDel, list) => {
+    // Навешиваем событие на кнопку "Удалить"
+    btnDel.addEventListener('click', () => {
+      // Проходим по таблице, находим элементы с классом delete и показываем их
+      document.querySelectorAll('.delete').forEach(del => {
+        del.classList.toggle('is-visible');
+      });
     });
-  });
 
-  // С помощью делегирования m,eltv кликать по list - это вся наша область таблицы
-  list.addEventListener('click', event => {
+    // С помощью делегирования m,eltv кликать по list - это вся наша область таблицы
+    list.addEventListener('click', event => {
     const target = event.target // Назначаем переменную, что бы дальне не писать event.target
     // contains используют если кнопка в единственном виде без содержимого 
     // closest используют когда внутри элемента есть еще элемент. Например, внутри кнопки svg
-    if (target.closest('.del-icon')) {
+      if (target.closest('.del-icon')) {
       // находим родителя у event.target и удаляем его
       target.closest('.contact').remove();
     }
   });
 };
+  // Функция обрабатывает форму
+  const formControl = (form, closeModal) => {
+  // Вешаем событе на нажатие кнопки "Добавить" в форме
+    form.addEventListener('submit', e => {
+      // Убираем стандартную перезагрузку страницы при нажатии на кнопку "добавить"
+      e.preventDefault();
+
+      // Добавляем очистку формы  после того как она отработает
+      form.reset();
+      closeModal();
+    });
+  };
 
 
  // Ф-я, которая инициализирует наше приложение
@@ -389,13 +412,14 @@ btnDel.addEventListener('click', () => {
     // Функционал
     //В ф-ю передаем list в чистом виде после деструктуризации и data
     const allRow = renderContacts(list, data);
+    // Вызываем ф-ю открытия модального окна при нажатии кн "Добавить"
+    const {closeModal} = modalControl(btnAdd, formOverlay);
     // Вызываем функцию, передаем allRow и logo?  иначе не сможем с ними взаимодействовать
     hoverRow(allRow, logo);
-    // Вызываем ф-ю открытия модального окна при нажатии кн "Добавить"
-    modalControl(btnAdd, formOverlay);
     // Вызываем ф-ю проявления "крестиков" при нажатии кнопки "удалить"
     deleteControl(btnDel, list);
-    formControl(form);
+    // Принимает деструктурированную форму
+    formControl(form, closeModal);
 
     // Взаимодействие с тачскринами мобильных устройств
     // Аналог mousedown (прикосновение к DOM элементу)
